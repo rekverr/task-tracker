c914c97e486773cf8b69ed5f6d419bef3d2bc316
Viktor Schehelskyi <victorshegelsky@gmail.com>
2026-08-10 12:49:03 +0300

    feat(backend): setup jwt passport strategy, guard and user decorator

---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_ACCESS_SECRET || 'fallback_secret',
+    });
+  }
+
+  async validate(payload: { sub: string; email: string }) {
+    return { id: payload.sub, email: payload.email };
+  }
+}
