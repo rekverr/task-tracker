6fffb4684e01612ac5c2df17e0d4ec1f645c4338
Viktor Schehelskyi <victorshegelsky@gmail.com>
2026-08-10 12:37:49 +0300

    feat(backend): create users module and service

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async findById(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+}
