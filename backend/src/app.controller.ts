7018e5ad52b61fbfc914657de506a6148f5ce6ba
Viktor Schehelskyi <victorshegelsky@gmail.com>
2026-08-11 11:06:07 +0300

    refactor(backend): convert default app controller to health check endpoint

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
-@Controller()
+@ApiTags('Health')
+@Controller('health')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({ summary: 'Check API status' })
+  getHealth() {
+    return this.appService.getHealthCheck();
   }
 }
